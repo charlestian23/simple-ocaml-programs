1692ca07bb6637ef960bca8decc6e886c065ae6f
charlestian23 <charles.tian23@gmail.com>
2022-09-23 13:46:40 -0400

    Create factorial.ml

---
+++ factorial.ml
@@ -0,0 +1,9 @@
+(**
+  Factorial
+  =========
+  Given a number, x, from the command line, the program will print out x!
+*)
+
+let x = int_of_string Sys.argv.(1)
+let rec fact n = if n = 0 then 1 else n * fact (n - 1)
+let _ = print_endline (string_of_int (fact x))
