493b50f6ff0b55c9195bf752b9a4751075d765b7
charlestian23 <charles.tian23@gmail.com>
2022-09-28 16:25:51 -0400

    Adjusted formatting

--- factorialpm.ml
+++ factorialpm.ml
@@ -5,7 +5,9 @@
 *)
 
 let x = int_of_string Sys.argv.(1)
-let rec factorial = function 
-  | 0 | 1 -> 1 
+
+let rec factorial = function
+  | 0 | 1 -> 1
   | x -> x * factorial (x - 1)
+
 let _ = print_endline (string_of_int (factorial x))
