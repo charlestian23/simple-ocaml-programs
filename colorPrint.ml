8dc4c82f1599b0cf39af29bf33a6155bd0c34c0e
charlestian23 <charles.tian23@gmail.com>
2022-10-26 21:52:31 -0400

    Create colorPrint.ml

---
+++ colorPrint.ml
@@ -0,0 +1,4 @@
+print_endline "\027[91mThis sentence is in red"
+print_endline "\027[94mAnd this sentence is in blue"
+
+(* Reference for more colors: https://www.geeksforgeeks.org/print-colors-python-terminal/ *)
